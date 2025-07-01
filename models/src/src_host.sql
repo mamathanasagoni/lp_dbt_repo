040fd1dd64df28206681087f033f121f40b7e608
lpmamatha22@gmail.com <lpmamatha22@gmail.com>
2025-07-01 09:52:24 +0000

    commit stagging models

---
+++ models/src/src_host.sql
@@ -0,0 +1,16 @@
+src_hosts.sql as
+
+WITH raw_hosts AS (
+    SELECT
+        *
+    FROM
+       DATA_ENG_DBT.RAW.RAW_HOSTS
+)
+SELECT
+    id AS host_id,
+    NAME AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM
+    raw_hosts
